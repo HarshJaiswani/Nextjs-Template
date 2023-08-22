4d4fc0305a66c64128bce1390345d715b5288b17
Harsh <harshjaiswani2003@gmail.com>
2023-08-22 19:15:52 +0530

    Removed useless apis

--- pages/api/all_users.js
+++
@@ -1,9 +0,0 @@
-import connectDb from "middleware/connectDB";
-import Users from "models/User";
-
-const handler = async (req, res) => {
-  let user = await Users.find();
-  res.status(200).json({ user });
-};
-
-export default connectDb(handler);
